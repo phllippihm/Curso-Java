b0c0665558a2387b6b237bb9b3492c5735ecfe7c
Philippi Harger Martins <81309714+phllippihm@users.noreply.github.com>
2022-09-21 00:26:46 -0300

    Add files via upload

---
+++ cursoClasseDesafio/Pessoa.java
@@ -0,0 +1,23 @@
+package cursoClasseDesafio;
+
+public class Pessoa {
+	
+	String nome;
+	double peso;
+	
+	Pessoa(String nome, double peso){
+		this.nome = nome;
+		this.peso = peso;
+	}
+	
+	void comer(Comida comida) {
+		if(comida !=null) {
+			this.peso += comida.peso;
+		}
+	}
+	
+	String apresentar() {
+		return "Olá eu sou o " + nome + " e tenho " + peso + " Kgs.";
+	}
+
+}
